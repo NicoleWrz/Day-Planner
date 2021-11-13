36a229633529195ac072331f88ec6b0d0ef3c0e8
NicoleWrz <nicolew96@gmail.com>
2021-11-13 00:57:48 -0500

    adding javascript for time

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,19 @@
 var today = moment();
 $("#todays-date").text(today.format("dddd, MMM Do, YYYY"));
 
+var hours = document.querySelectorAll(".hour")
+
+for (var i = 0; i < hours.length; i++) {
+    console.log(hours[i].textContent)
+    var timeString = hours[i].textContent;
+    var timeHours = moment(timeString, "hhA");
+    today.isAfter(timeHours);
+    console.log(today.isAfter(timeHours))
+    //is same
+    //is before
+    //if statements
+    //jquery add css to from div to textarea
+}
+
+//local storage
+
